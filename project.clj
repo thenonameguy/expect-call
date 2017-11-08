5d09ea7d7d3347c8183eeec01a6627276787bfe2
Krisztian Szabo <kszabo@whitepages.com>
2017-11-08 15:00:12 +0100

    Bump to 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject the_nonameguy/expect-call "0.1.2"
+(defproject the_nonameguy/expect-call "0.1.3"
   :description "A Clojure library for no-fuss function mocking"
   :url "https://github.com/meredydd/expect-call"
   :license {:name "Eclipse Public License"
