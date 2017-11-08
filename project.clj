c3ebdc972a78835b715bb7a4ad7d74e908d09b69
Krisztian Szabo <kszabo@whitepages.com>
2017-11-08 14:08:18 +0100

    Release 0.1.1 under own name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.senatehouse/expect-call "0.1.0"
+(defproject the_nonameguy/expect-call "0.1.1"
   :description "A Clojure library for no-fuss function mocking"
   :url "https://github.com/meredydd/expect-call"
   :license {:name "Eclipse Public License"
